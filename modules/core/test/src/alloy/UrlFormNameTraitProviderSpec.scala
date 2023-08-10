9f8c713e8aa635c5a01e377a26fc444225efcffb
David Piggott <david@piggott.me.uk>
2023-08-10 11:00:11 +0100

    Add UrlFormNameTraitProviderSpec

--- modules/core/test/src/alloy/UrlFormNameTraitProviderSpec.scala
+++ modules/core/test/src/alloy/UrlFormNameTraitProviderSpec.scala
@@ -15,7 +15,6 @@
 
 package alloy
 
-import software.amazon.smithy.model.shapes.ListShape
 import software.amazon.smithy.model.shapes.MemberShape
 import software.amazon.smithy.model.shapes.ShapeId
 import software.amazon.smithy.model.shapes.StringShape
@@ -23,20 +22,15 @@ import software.amazon.smithy.model.shapes.StructureShape
 import software.amazon.smithy.model.Model
 import java.util.Optional
 
-final class UrlFormFlattenedTraitProviderSpec extends munit.FunSuite {
+final class UrlFormNameTraitProviderSpec extends munit.FunSuite {
 
   test("has trait") {
     val stringShape = StringShape
       .builder()
       .id(ShapeId.fromParts("test", "MyString"))
       .build()
-    val listShape = ListShape
-      .builder()
-      .id(ShapeId.fromParts("test", "MyList"))
-      .member(stringShape.getId)
-      .build()
     val structId = ShapeId.fromParts("test", "MyStruct")
-    val targetId = structId.withMember("myList")
+    val targetId = structId.withMember("myString")
     val structShape = StructureShape
       .builder()
       .id(structId)
@@ -44,22 +38,57 @@ final class UrlFormFlattenedTraitProviderSpec extends munit.FunSuite {
         MemberShape
           .builder()
           .id(targetId)
-          .target(listShape.getId)
-          .addTrait(new UrlFormFlattenedTrait)
+          .target(stringShape.getId)
+          .addTrait(UrlFormNameTrait.builder().setName("my_string").build())
           .build()
       )
       .build()
 
     val model =
       Model.assembler.disableValidation
-        .addShapes(structShape, listShape, stringShape)
+        .addShapes(structShape, stringShape)
         .assemble()
         .unwrap()
 
     val result = model.getShape(targetId).map(shape =>
-      shape.hasTrait(classOf[UrlFormFlattenedTrait])
+      shape.hasTrait(classOf[UrlFormNameTrait])
     )
 
     assertEquals(result, Optional.of(true))
+  }
+
+  test("trait has name") {
+    val stringShape = StringShape
+      .builder()
+      .id(ShapeId.fromParts("test", "MyString"))
+      .build()
+    val structId = ShapeId.fromParts("test", "MyStruct")
+    val targetId = structId.withMember("myString")
+    val structShape = StructureShape
+      .builder()
+      .id(structId)
+      .addMember(
+        MemberShape
+          .builder()
+          .id(targetId)
+          .target(stringShape.getId)
+          .addTrait(UrlFormNameTrait.builder().setName("my_string").build())
+          .build()
+      )
+      .build()
+
+    val model =
+      Model.assembler.disableValidation
+        .addShapes(structShape, stringShape)
+        .assemble()
+        .unwrap()
+
+    val result = model.getShape(targetId).flatMap(shape =>
+      shape.getTrait(classOf[UrlFormNameTrait])
+    ).map(urlFormNameTrait =>
+      urlFormNameTrait.getName()
+    )
+
+    assertEquals(result, Optional.of("my_string"))
   }
 }
