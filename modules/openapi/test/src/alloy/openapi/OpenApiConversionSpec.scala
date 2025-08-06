a765c9f87644700193b4a3184eb562bcc8521dc4
Jakub Kozłowski <kubukoz@gmail.com>
2025-08-06 20:20:42 +0200

    don't check exit code

--- modules/openapi/test/src/alloy/openapi/OpenApiConversionSpec.scala
+++ modules/openapi/test/src/alloy/openapi/OpenApiConversionSpec.scala
@@ -61,7 +61,7 @@ final class OpenApiConversionSpec extends munit.FunSuite {
         createFolders = true
       )
 
-      val diffText = os.proc("diff", "-u", expectedFile.toString, tmp.toString).call().out.text()
+      val diffText = os.proc("diff", "-u", expectedFile.toString, tmp.toString).call(check = false).out.text()
 
       fail(
         s"Values are not the same. Wrote current output to $tmp for easier debugging. Diff:\n$diffText"
